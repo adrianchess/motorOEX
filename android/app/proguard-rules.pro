9d1f393667b889e423b4fd7e50b3541a9c7e1ab1
adrianchess <189170481+adrianchess@users.noreply.github.com>
2026-05-27 20:48:59 +0200

    Remove deprecated edge-to-edge calls from release

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -1,2 +1,8 @@
 -keep class com.analisismotoresoex.motoresoex.MainActivity { public <init>(); }
 -keep class com.analisismotoresoex.motoresoex.OexEngineProvider { public <init>(); }
+
+-assumenosideeffects class android.view.Window {
+    public void setStatusBarColor(int);
+    public void setNavigationBarColor(int);
+    public void setNavigationBarDividerColor(int);
+}
--- android/app/src/main/kotlin/com/analisismotoresoex/motoresoex/MainActivity.kt
+++ android/app/src/main/kotlin/com/analisismotoresoex/motoresoex/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.analisismotoresoex.motoresoex
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterFragmentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
-		enableEdgeToEdge()
 		super.onCreate(savedInstanceState)
+		enableAndroid15EdgeToEdge()
+	}
+
+	override fun onPostResume() {
+		super.onPostResume()
+		enableAndroid15EdgeToEdge()
 	}
 
 	override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
@@ -23,6 +29,19 @@ class MainActivity : FlutterFragmentActivity() {
 				"getEngineStatus" -> result.success(OexEngineRegistry.status(this))
 				else -> result.notImplemented()
 			}
+		}
+	}
+
+	private fun enableAndroid15EdgeToEdge() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+			window.setDecorFitsSystemWindows(false)
+		}
+
+		if (Build.VERSION.SDK_INT >= 35) {
+			val attributes = window.attributes
+			attributes.layoutInDisplayCutoutMode =
+				WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+			window.attributes = attributes
 		}
 	}
 }
